4a472f10cdf07f9de5abb21a656a67db1b99cec8
Jens Rasmussen <stamppot@gmail.com>
2016-04-12 22:28:09 +0200

    Sphinx enabled

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -11,7 +11,7 @@
 # include_recipe "deny-directories"
 
 # uncomment to turn on thinking sphinx 2/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
-# include_recipe "sphinx"
+include_recipe "sphinx"
 
 # uncomment to turn on thinking sphinx 3. See cookbooks/thinking-sphinx-3/readme.md for documentation.
 # include_recipe "thinking-sphinx-3"
--- cookbooks/sphinx/recipes/default.rb
+++ cookbooks/sphinx/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 
 # Set your application name here
-appname = "myapp"
+appname = "CBCL"
 
 # Uncomment the flavor of sphinx you want to use
 flavor = "thinking_sphinx"
@@ -27,7 +27,7 @@ utility_name = nil
 # If you don't want scheduled reindexes, just leave this set to nil.
 # Setting it equal to 10 would run the cron job every 10 minutes.
 
-cron_interval = nil #If this is not set your data will NOT be indexed
+cron_interval = 10 #If this is not set your data will NOT be indexed
 
 
 if ! File.exists?("/data/#{appname}/current")
